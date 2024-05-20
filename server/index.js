6f44a8cb70579f48a9c90f247c5bb49b5c4e38d9
Nicolas Lebon <niko83136@gmail.com>
2024-05-20 21:46:52 +0200

    Fix db populating automation

--- server/index.js
+++ server/index.js
@@ -14,13 +14,6 @@ const Bench = require('./models/bench')(sequelize, DataTypes);
 const User = require('./models/user')(sequelize, DataTypes);
 const Equipment = require('./models/equipment')(sequelize, DataTypes);
 
-const room = Room.create({ name: 'Room 1' });
-const project = Project.create({ name: 'Project 1' });
-const bench = Bench.create({ name: 'Bench 1', RoomId: room.id });
-const user = User.create({ firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com' });
-const equipment = Equipment.create({ name: 'Equipment 1' });
-
-
 sequelize.authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
@@ -31,9 +24,11 @@ sequelize.authenticate()
 
 // Synchroniser tous les modèles
 sequelize.sync()
-  .then(() => {
-    console.log('Database & tables created!');
+  .then(async () => {
+    //console.log('Database & tables created!');
+    populateDb()
   });
+
 
 app.get('/users', async (req, res) => {
   //res.send('Hello world!');
@@ -41,7 +36,13 @@ app.get('/users', async (req, res) => {
   res.json(users)
 });
 
-
+async function populateDb() {
+  const room = await Room.create({ name: 'Room 1' });
+  Project.create({ name: 'Project 1' });
+  Bench.create({ name: 'Bench 1', RoomId: room.id });
+  User.create({ firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com' });
+  Equipment.create({ name: 'Equipment 1' });
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
