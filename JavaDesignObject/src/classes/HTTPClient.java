2668387e0b7295efb7f3775846ed973dad50083f
Jeremy <jeremyb.lebon@gmail.com>
2024-05-25 11:11:07 +0200

    Appel des routes à l'instanciation de la classe

--- JavaDesignObject/src/classes/HTTPClient.java
+++ JavaDesignObject/src/classes/HTTPClient.java
@@ -4,16 +4,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HTTPClient {
-    public static void main(String[] args) {
+
+    public HTTPClient(List<String> pathList) {
         try {
-            String response = getHttpResponse("users", 3000);
-            System.out.println("Response: " + response);
+            pathList.forEach(path -> {
+                try {
+                    String response = getHttpResponse(path,3000);
+                    System.out.println(path +" : " + response);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-}
+    }
+
     // Fonction générique pour obtenir la réponse d'une requête HTTP GET
     public static String getHttpResponse(String object, int port) throws Exception {
         // Créer l'objet URL
@@ -42,6 +54,12 @@ public class HTTPClient {
 
         // Retourner la réponse sous forme de chaîne de caractères
         return response.toString();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> pathList;
+        new HTTPClient(pathList = new ArrayList<>(Arrays.asList("benches", "projects", "rooms", "users")));
+
     }
 
 }
