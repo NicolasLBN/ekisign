de00ff0edec976f24a3bed9504142122e80582e0
Jeremy <jeremyb.lebon@gmail.com>
2024-05-25 11:14:44 +0200

    Ajout des routes des differents items de la DB

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ sequelize.sync()
     populateDb()
   });
 
-
+// Route pour récupérer tous les users
 app.get('/users', async (req, res) => {
   try {
     const users = await User.findAll();
@@ -41,6 +41,48 @@ app.get('/users', async (req, res) => {
     res.status(500).json({ error: 'Failed to retrieve users' });
   }
 });
+
+// Route pour récupérer tous les bancs
+app.get('/benches', async (req, res) => {
+  try {
+    const benches = await Bench.findAll();
+    res.json(benches);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve benches' });
+  }
+});
+
+// Route pour récupérer tous les projets
+app.get('/projects', async (req, res) => {
+  try {
+    const projects = await Project.findAll();
+    res.json(projects);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve projects' });
+  }
+});
+
+// Route pour récupérer toutes les salles
+app.get('/rooms', async (req, res) => {
+  try {
+    const rooms = await Room.findAll();
+    res.json(rooms);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve rooms' });
+  }
+});
+
+// Route pour récupérer toutes les equipements
+app.get('/equipements', async (req, res) => {
+  try {
+    const device = await Room.findAll();
+    res.json(device);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to retrieve rooms' });
+  }
+});
+
+
 
 async function populateDb() {
   const room = await Room.create({ name: 'Room 1' });
