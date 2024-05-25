61d967ce15e072cd83244f598f49798bfd951ee3
Jeremy <jeremyb.lebon@gmail.com>
2024-05-25 10:21:52 +0200

    Implementation getHttpResponse()

--- JavaDesignObject/src/classes/HTTPClient.java
+++ JavaDesignObject/src/classes/HTTPClient.java
@@ -8,37 +8,42 @@ import java.net.URL;
 public class HTTPClient {
     public static void main(String[] args) {
         try {
-            // URL de la route du serveur Node.js
-            String url = "http://localhost:3000/users";
-
-            // Créer l'objet URL
-            URL obj = new URL(url);
-
-            // Ouvrir la connexion
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-            // Définir la méthode de la requête (GET)
-            con.setRequestMethod("GET");
-
-            // Obtenir le code de réponse
-            int responseCode = con.getResponseCode();
-            System.out.println("Response Code : " + responseCode);
-
-            // Lire la réponse
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // Imprimer la réponse
-            System.out.println("Response : " + response.toString());
-
+            String response = getHttpResponse("users", 3000);
+            System.out.println("Response: " + response);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
 }
+    // Fonction générique pour obtenir la réponse d'une requête HTTP GET
+    public static String getHttpResponse(String object, int port) throws Exception {
+        // Créer l'objet URL
+        String strUrl = "http://localhost:" + Integer.toString(port) +"/" + object;
+        URL url = new URL(strUrl);
+
+        // Ouvrir la connexion
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        // Définir la méthode de la requête (GET)
+        con.setRequestMethod("GET");
+
+        // Obtenir le code de réponse
+        int responseCode = con.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        // Lire la réponse
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        // Retourner la réponse sous forme de chaîne de caractères
+        return response.toString();
+    }
+
+}
+
+
