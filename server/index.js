871b4131f2322eee83fe803332db03d311544df4
Nicolas LEBON <niko83136@gmail.com>
2024-09-16 12:48:41 +0200

    Implement CORS policy and update draw.io

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const { Sequelize, DataTypes } = require('sequelize');
 const bodyParser = require('body-parser');
 const object = ["rooms", "users", "benches", "projects", "equipments"];
@@ -9,6 +11,8 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
 
 // Set up Sequelize
 const sequelize = new Sequelize('ekisign', 'postgres', 'root', {
